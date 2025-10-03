2b1e0c2c3f7cabd0d62806debaaa84373d051b4b
TimurTimergalin <timur20041229@gmail.com>
2025-10-03 23:56:28 +0300

    Update script

--- make_index.py
+++ make_index.py
@@ -173,7 +173,7 @@ def validate_files(files):
         else:
             real_content = f.read()
             if f.read() != file.content:
-                diff = '\n'.join(difflib.unified_diff(real_content.split('\n'), file.content.split('\n')))
+                diff = ' '.join(difflib.unified_diff(real_content.split('\n'), file.content.split('\n')))
                 raise RuntimeError(
                     f"{file.path} has invalid content. You seem to have forgotten to remake the index\ndiff:\n{diff}"
                 )
